6b8d5dc51b728f8cd42cef6f6e29d982ad53217c
Harry Burnett <stageosu@gmail.com>
2021-12-23 23:39:41 -0500

    Reverted class rename

--- src/core/logger.py
+++ src/core/logger.py
@@ -3,7 +3,7 @@ import sys
 from logging import FileHandler, StreamHandler, Formatter
 
 
-class Loggers:
+class Logger:
     """Class for logging messages to stdout and to log files. Contains different types of loggers
     as class variables that will all point to the specified log_dir."""
     def __init__(self, log_dir):
