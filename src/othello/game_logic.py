ab4e7b6bb64cf2e8c21d026ec506fda750946e5f
Harry Burnett <stageosu@gmail.com>
2021-12-24 11:07:17 -0500

    Expanded BitBoard class and tests

--- src/othello/game_logic.py
+++ src/othello/game_logic.py
@@ -1,8 +1,18 @@
 import numpy as np
+from core import config, logger
 from othello import color
+
+
+class Move:
+    def __init__(self, c, pos=-1, is_pass=True):
+        self.color = c
+        self.pos = pos
+        self.is_pass = is_pass
+
 
 BLACK_BITS = np.uint64(0x0000000810000000)
 WHITE_BITS = np.uint64(0x0000001008000000)
+
 
 class BitBoard:
     def __init__(self, c, bits=np.uint64(0)):
@@ -15,3 +25,31 @@ class BitBoard:
                 self.bits = BLACK_BITS
             else:
                 self.bits = WHITE_BITS
+
+    def __repr__(self):
+        return f'BitBoard(color={self.color} ({color.as_str(self.color)}, bits={self.bits} ({np.binary_repr(self.bits)}))'
+
+    def set_bit(self, pos) -> None:
+        """
+        Sets the bit at the desired position to 1.
+        :param pos: Bit position to set. Range 0 to 63 inclusive. Throws ValueError if pos is outside that bound.
+        """
+        if pos < 0 or pos > 63:
+            raise ValueError(f'Expected pos between 0 and 63 inclusive. Recieved {pos}')
+
+        mask = np.uint64(1 << pos)
+        self.bits |= mask
+
+    def get_bit_state(self, p):
+        mask = np.uint64(1 << p)
+        return (self.bits & mask) != 0
+
+    def disable_bit(self, p):
+        mask = np.uint64(1 << p)
+        self.bits &= ~mask
+
+    def bitcount(self):
+        return self.binary_repr().count('1')
+
+    def binary_repr(self) -> str:
+        return np.binary_repr(self.bits, 64)
---
+++ src/test/test_game_logic.py
@@ -0,0 +1,69 @@
+import unittest
+import numpy as np
+import othello.game_logic as game_logic
+
+from othello.game_logic import Move, BitBoard
+from othello import color
+
+VALID_POSITIONS = [x for x in range(64)]
+
+
+class TestMove(unittest.TestCase):
+    def test_default(self):
+        move = Move(color.BLACK)
+        self.assertTrue(move.is_pass, f'Expected default behavior to be pass. Received {move}')
+
+
+class TestBitBoard(unittest.TestCase):
+    def test_init(self):
+        bb_black = BitBoard(color.BLACK)
+        bb_white = BitBoard(color.WHITE)
+
+        self.assertTrue(bb_black.bits == game_logic.BLACK_BITS)
+        self.assertTrue(bb_white.bits == game_logic.WHITE_BITS)
+
+    def test_set_bit(self):
+        for p in VALID_POSITIONS:
+            copy = BitBoard(color.BLACK)
+            copy.set_bit(p)
+            reference = game_logic.BLACK_BITS | np.uint64(1 << p)
+            self.assertTrue(copy.bits == reference)
+
+        bad = [-1, 65]
+        for p in bad:
+            copy = BitBoard(color.BLACK)
+            self.assertRaises(ValueError, copy.set_bit, p)
+
+    def test_get_bit_state(self):
+        for p in VALID_POSITIONS:
+            copy = BitBoard(color.BLACK)
+            copy.set_bit(p)
+            self.assertTrue(copy.get_bit_state(p))
+
+    def test_disable_bit(self):
+        for p in VALID_POSITIONS:
+            copy = BitBoard(color.BLACK)
+            copy.disable_bit(p)
+            self.assertFalse(copy.get_bit_state(p), f'Expected bit to be disabled. Received {copy.get_bit_state(p)} instead. '
+                                                    f'Pos: {p} | Bits: {copy.binary_repr()}')
+
+    def test_bitcount(self):
+        # Initial count
+        copy = BitBoard(color.BLACK)
+        self.assertTrue(copy.bitcount() == 2, f'Expected bitcount to equal 2. Received {copy.bitcount()}')
+
+        # All positions occupied by one color would never occur in a real game.
+        for p in VALID_POSITIONS:
+            copy.set_bit(p)
+
+        self.assertTrue(copy.bitcount() == 64)
+
+        # Disable all
+        for p in VALID_POSITIONS:
+            copy.disable_bit(p)
+
+        self.assertTrue(copy.bitcount() == 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
