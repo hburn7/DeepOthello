c75bf821c4a3730dbb4bf1929be3213c8ae1fb63
Harry Burnett <stageosu@gmail.com>
2021-12-23 23:39:21 -0500

    Cleanup

--- src/test/test_args.py
+++ src/test/test_args.py
@@ -1,27 +1,34 @@
 import unittest
-from core import parse_args, cfg
+from core import parse_args
+from core.config import Config
+
 
 class TestArgs(unittest.TestCase):
     def test_program_args(self):
         args = ['-ref', '-t 600', '-c 1', '-i', '-g', '-l test_logs']
-        config = cfg(args)
-        self.assertTrue(config.ref_compatibility)
-        self.assertTrue(config.ai_game_time == 600)
-        self.assertTrue(config.ai_color == 1)
-        self.assertTrue(config.interactive)
-        self.assertTrue(config.gui)
-        self.assertTrue(config.log_dir == 'test_logs')
+        parsed = parse_args(args)
+        cfg = Config(referee_compatibility=parsed.referee_compatible, ai_game_time=parsed.ai_game_time,
+                     ai_color=parsed.ai_color,
+                     interactive=parsed.interactive, gui=parsed.gui, log_dir=parsed.log_dir)
+        self.assertTrue(cfg.ref_compatibility)
+        self.assertTrue(cfg.ai_game_time == 600)
+        self.assertTrue(cfg.ai_color == 1)
+        self.assertTrue(cfg.interactive)
+        self.assertTrue(cfg.gui)
+        self.assertTrue(cfg.log_dir == 'test_logs')
 
     def test_invalid_color(self):
         bad_args = ['-c 0', '-c -2', '-c 2']
 
         for a in bad_args:
             try:
-                _ = cfg(a)
+                parsed = parse_args(a)
+                _ = Config(ai_color=parsed.ai_color)
             except:
                 continue
 
             self.assertTrue(False, f'Expected exception.')
+
 
 if __name__ == '__main__':
     unittest.main()
