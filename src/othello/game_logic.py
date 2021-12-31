a13ab8bd82ca1e2d471a4d1e6155fe32623d5ec9
Harry Burnett <stageosu@gmail.com>
2021-12-31 12:06:08 -0500

    Created GameBoard

--- src/othello/game_logic.py
+++ src/othello/game_logic.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from othello import color
+from core import config, logger
 
 
 class Move:
@@ -8,6 +9,9 @@ class Move:
         self.color = c
         self.pos = pos
         self.is_pass = is_pass
+
+    def __repr__(self):
+        return f'Move(c={self.color} ({color.as_str(self.color)}), pos={self.pos}, is_pass={self.is_pass})'
 
 
 BLACK_BITS = np.uint64(0x0000000810000000)
@@ -53,3 +57,30 @@ class BitBoard:
 
     def binary_repr(self) -> str:
         return np.binary_repr(self.bits, 64)
+
+
+class GameBoard:
+    DIR_COUNT = 8
+    UNIVERSE = np.uint64(0xffffffffffffffff)
+
+    DIR_INCREMENTS = [8, 9, 1, -7, -8, -9, -1, 7]
+    DIR_MASKS = np.array([
+        0xFFFFFFFFFFFFFF00,  # North
+        0xFEFEFEFEFEFEFE00,  # NorthWest
+        0xFEFEFEFEFEFEFEFE,  # West
+        0x00FEFEFEFEFEFEFE,  # SouthWest
+        0x00FFFFFFFFFFFFFF,  # South
+        0x007F7F7F7F7F7F7F,  # SouthEast
+        0x7F7F7F7F7F7F7F7F,  # East
+        0x7F7F7F7F7F7F7F00  # NorthEast
+    ], dtype=np.uint64)
+
+    def __init__(self, player_board: BitBoard, opp_board: BitBoard):
+        self.p_color = player_board.color
+        self.o_color = opp_board.color
+        self.player_board = player_board
+        self.opp_board = opp_board
+        self.move_history = []  # List[Move] (we will use this as a Stack)
+
+    def __repr__(self):
+        return f'GameBoard(player_board={self.player_board}, opp_board={self.opp_board})'
